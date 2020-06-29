85d211d0b89de33c32429a6dec728c9a17d080bc
Louis Quinnell <_@ziorobo.co>
2020-06-29 22:18:56 +1000

    Create basic scene

--- client/index.ts
+++ client/index.ts
@@ -1,6 +1,59 @@
-const root = document.createElement("div")
-document.body.appendChild(root)
+import {
+  ArcRotateCamera,
+  Color3,
+  Color4,
+  Engine,
+  HemisphericLight,
+  Mesh,
+  MeshBuilder,
+  Scene,
+  Vector3,
+} from "babylonjs"
 
-import("../pkg").then(engine => {
-  root.innerHTML = `<h1 style="text-align: center;">${engine.message()}</h1>`
+const canvas = document.createElement("canvas")
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+document.body.appendChild(canvas)
+
+const antialiasEnabled = true
+
+const engine = new Engine(canvas, antialiasEnabled)
+
+const createScene = (): Scene => {
+  const scene = new Scene(engine)
+  scene.clearColor = Color4.FromColor3(Color3.Black())
+
+  const camera = new ArcRotateCamera(
+    "camera",
+    Math.PI / 2,
+    Math.PI / 2,
+    2,
+    Vector3.Zero(),
+    scene
+  )
+
+  var light: HemisphericLight = new HemisphericLight(
+    "light",
+    new Vector3(1, 1, 0),
+    scene
+  )
+
+  var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene)
+
+  return scene
+}
+
+const scene = createScene()
+
+engine.runRenderLoop(() => {
+  scene.render()
 })
+
+window.addEventListener("resize", () => {
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+  engine.resize
+})
+
+import("../pkg").then(engine => {})
