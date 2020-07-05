e6b1f25030c57f3167cdcca2bcbbc6c3361a9008
Louis Quinnell <_@ziorobo.co>
2020-07-05 18:28:34 +1000

    Use wasd for camera input

--- client/src/camera.ts
+++ client/src/camera.ts
@@ -1,5 +1,6 @@
 import { Scene } from "@babylonjs/core/scene"
 import { UniversalCamera } from "@babylonjs/core/Cameras/universalCamera"
+import { codes } from "keycode"
 
 import { State } from "./types"
 
@@ -7,5 +8,9 @@ export const initCamera = (state: State, scene: Scene): UniversalCamera => {
   const camera = new UniversalCamera("camera", state.camera.position, scene)
   camera.setTarget(state.camera.target)
   camera.speed = 0.2
+  camera.keysUp = [codes.w]
+  camera.keysDown = [codes.s]
+  camera.keysLeft = [codes.a]
+  camera.keysRight = [codes.d]
   return camera
 }
