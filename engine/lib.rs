edbd2dfe0818678e1d13f58659313dbfe726f16d
Louis Quinnell <_@ziorobo.co>
2020-07-19 23:38:55 +1000

    Return a view into wasm instance memory

--- engine/lib.rs
+++ engine/lib.rs
@@ -1,9 +1,13 @@
 #![allow(unused_unsafe)]
 
+use js_sys::Uint8Array;
 use wasm_bindgen::prelude::*;
 use web_sys::console;
 
 const TRIANGLE: &str = "△";
+
+const BUFFER_SIZE: usize = 8;
+static mut BUFFER: [u8; BUFFER_SIZE] = [0; BUFFER_SIZE];
 
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
@@ -12,9 +16,21 @@ pub fn main() -> Result<(), JsValue> {
 
     unsafe {
         console::log_1(&JsValue::from_str(TRIANGLE));
+        for n in 0..BUFFER_SIZE {
+            BUFFER[n] = 10 + (n as u8);
+            console::log_1(&JsValue::from_str(&format!(
+                "{} -> BUFFER[{}]",
+                BUFFER[n], n
+            )));
+        }
     }
 
     Ok(())
+}
+
+#[wasm_bindgen]
+pub fn view() -> Uint8Array {
+    unsafe { js_sys::Uint8Array::view(&BUFFER) }
 }
 
 #[wasm_bindgen]
