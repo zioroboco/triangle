85b3547081ee3c0b3b9ca45793f85fd94a0ee917
Louis Quinnell <_@ziorobo.co>
2020-08-23 01:27:15 +1000

    Rename setup module

--- client/src/setup.ts
+++ client/src/setup.ts
@@ -4,7 +4,7 @@ import { range } from "ramda"
 
 const rand = (min: number, max: number) => Math.random() * (max - min) + min
 
-export const swarm = (length = N): [Float32Array, Float32Array] => {
+const swarm = (length = N): [Float32Array, Float32Array] => {
   let ps: number[] = []
   let vs: number[] = []
   range(0, length).forEach(i => {
@@ -16,26 +16,6 @@ export const swarm = (length = N): [Float32Array, Float32Array] => {
     const vel_theta = Matrix.RotationAxis(Vector3.Up(), rand(0.1, 0.2))
     const vel = Vector3.TransformCoordinates(
       Vector3.Cross(pos, Vector3.Up()).scale(rand(0.3, 0.6)),
-      vel_theta
-    )
-
-    ps[i * DIM_N + DIM_X] = pos.x
-    ps[i * DIM_N + DIM_Y] = pos.z
-    vs[i * DIM_N + DIM_X] = vel.x
-    vs[i * DIM_N + DIM_Y] = vel.z
-  })
-  return [new Float32Array(ps), new Float32Array(vs)]
-}
-
-export const line = (length = N): [Float32Array, Float32Array] => {
-  let ps: number[] = []
-  let vs: number[] = []
-  range(0, length).forEach(i => {
-    const pos = Vector3.Forward().scale(2 + i * 0.0006)
-
-    const vel_theta = Matrix.RotationAxis(Vector3.Up(), 1)
-    const vel = Vector3.TransformCoordinates(
-      Vector3.Cross(pos, Vector3.Up()),
       vel_theta
     )
 
