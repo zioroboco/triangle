ce62e6788cb3fe20aec6df1ecfe9d21e37d2bf3f
Louis Quinnell <_@ziorobo.co>
2020-08-23 12:49:41 +1000

    Add worker-loader

--- webpack.config.ts
+++ webpack.config.ts
@@ -20,6 +20,12 @@ const config = (env: any): Configuration => {
     module: {
       rules: [
         {
+          test: /\.worker.ts$/,
+          use: {
+            loader: "worker-loader",
+          },
+        },
+        {
           test: /\.tsx?$/,
           use: [
             {
